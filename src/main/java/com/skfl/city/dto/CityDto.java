166c07af9714ed5f9cae649db8b12e86038883e9
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-08-23 17:56:39 +0400

    Make DTO validation to match DB constraints
    Closes review issue

--- src/main/java/com/skfl/city/dto/CityDto.java
+++ src/main/java/com/skfl/city/dto/CityDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
@@ -12,13 +13,16 @@ public class CityDto {
 
     @NotBlank
     @Pattern(regexp = "^([a-zA-Z\\u0080-\\u024F]+(?:. |-| |'))*[a-zA-Z\\u0080-\\u024F]*$")
+    @Length(max = 255)
     @Schema(example = "Istanbul")
     private String name;
 
     @Pattern(regexp = "^([a-zA-Z\\u0080-\\u024F]+(?:. |-| |'))*[a-zA-Z\\u0080-\\u024F]*$")
+    @Length(max = 255)
     @Schema(example = "Turkey")
     private String countryName;
 
     @Schema(example = "uuid-logo-name-with-extension.jpg")
+    @Length(max = 255)
     private String logo;
 }
