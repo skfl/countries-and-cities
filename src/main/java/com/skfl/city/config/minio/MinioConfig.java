1f8c998d687b92258abd4a78c36a0bcb6a76864e
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-08-24 18:03:30 +0400

    Add minio file initialization

--- src/main/java/com/skfl/city/config/minio/MinioConfig.java
+++ src/main/java/com/skfl/city/config/minio/MinioConfig.java
@@ -1,12 +1,18 @@
 package com.skfl.city.config.minio;
 
 import io.minio.BucketExistsArgs;
+import io.minio.ListObjectsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.errors.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 @Log4j2
@@ -30,10 +36,19 @@ public class MinioConfig {
                 .endpoint(url)
                 .credentials(minioUsername, minioPassword)
                 .build();
+        createDefaultBucket(client);
+        if (client.listObjects(ListObjectsArgs.builder()
+                .bucket(bucketName)
+                .build()).iterator().hasNext()) {
+
+        }
+        return client;
+    }
+
+    private void createDefaultBucket(MinioClient client) throws ErrorResponseException, InsufficientDataException, InternalException, InvalidKeyException, InvalidResponseException, IOException, NoSuchAlgorithmException, ServerException, XmlParserException {
         if (!client.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
             client.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
             log.info("Minio bucket {} created successfully", bucketName);
         }
-        return client;
     }
 }
